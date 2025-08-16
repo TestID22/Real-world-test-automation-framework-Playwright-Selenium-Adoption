34cd05d9320ba56f76ce0b1697989c6e9da62b2d
22nd <vitaliiseptember@gmail.com>
2025-08-16 09:07:06 +0300

    added base classes

---
+++ framework/browser/BaseBrowserFactory.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class BrowserFactoryBase(ABC):
+    """Base class for browser factories with common functionality."""
+    """by abstract base class I established a contract to all that will use this base class"""
+
+
+    @abstractmethod
+    def get_browser_driver(self, browser=None, headless=False, **kwargs):
+        """Contract for getting a browser driver instance"""
+        pass
