ee43e34adb5a892ab3889b34931d3627ac13ed01
22nd <vitaliiseptember@gmail.com>
2025-10-23 10:50:07 +0300

    ADDED: - decorator for stabilization.py
    FIXED:
    -

--- configuration/utils/stabilization.py
+++ configuration/utils/stabilization.py
@@ -1,6 +1,13 @@
 
 
 def try_again(func, delay=3, attempts=5):
+    """
+    just an example of writing decorator.
+    :param func:
+    :param delay:
+    :param attempts:
+    :return:
+    """
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
     return wrapper
